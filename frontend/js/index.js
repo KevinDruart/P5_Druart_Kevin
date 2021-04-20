1998ef3e836f321dea2c636f018d7e513dff0c3d
KevinDruart <65339848+KevinDruart@users.noreply.github.com>
2021-04-20 11:27:27 +0200

    maj index

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,10 +1,45 @@
-/*On récupére la balise section "card"*/
-let card = document.getElementById("card");
+/*Appel de l'API*/
+const getTeddies = async function () {
+    const response = await fetch(url);
+    return await response.json();
+}
+getTeddies();
 
-/*création de l'affichage de la cartes des ours*/
-let produitContainer = create("div", "class", "Block");
-let produitP1 = create("div", "class", "P1");
-let produitP2 = create("div", "class", "P2");
-let produitP3 = create("div", "class", "P3");
-let produitP4 = create("div", "class", "P4");
-let produitP5 = create("div", "class", "P5");
+/*LISTE INDEX*/
+async function listeTeddies() {
+    const teddies = await getTeddies();
+
+    /*On selectionne la section "card"*/
+    let produits = document.getElementById("card");
+
+    /*On crée l'affichage des produits sur l'index*/
+    teddies.forEach((teddies) => {
+        /*on utilise notre fonction Create*/
+        let produitContainer = create("div", "class", "Block");
+        let produitB1 = create("div", "class", "B1");
+        let produitB2 = create("div", "class", "B2");
+        let produitNom = create("h2", "class", "Nomproduits");
+        let produitLien = create("a", "href", "produit.html?id=" + teddies._id);
+        let produitPrix = create("p", "class", "Prixproduit");
+        let produitImage = create("img", "src", teddies.imageUrl);
+
+    /*Attributs suplémentaires*/
+        produitImage.setAttribute("alt", "image du produit");
+        produitImage.setAttribute("class", "Imageproduit");
+        
+    /*on donne une hierarchie au elements*/ 
+        produits.appendChild(produitContainer);
+        produitContainer.appendChild(produitB1);
+        produitContainer.appendChild(produitB2);
+        produitB1.appendChild(produitImage);
+        produitB2.appendChild(produitNom);
+        produitB2.appendChild(produitPrix);
+        produitB2.appendChild(produitLien);
+
+    /*Attribution des données aux élements créees*/
+        produitNom.textContent = teddies.name;
+        produitPrix.textContent = teddies.price / 100 + " " + "euros";
+        produitLien.textContent = "Voir la description du produit.";
+    });
+};
+listeTeddies();
--- frontend/js/request.js
+++ frontend/js/request.js
@@ -1,10 +1,10 @@
-//recupérer les données 
-var request = new XMLHttpRequest();
-request.onreadystatechange = function() {
-    if(this.readyState == XMLHttpRequest.DONE && this.status == 200){
-        var response = JSON.parse(this.responseText);
-        console.log(response);
-    }
-};
-request.open("GET","http://localhost:3000/api/teddies");
-request.send();
+// URL API
+const url = "http://localhost:3000/api/teddies";
+const urlOrder = "http://localhost:3000/api/teddies/order";
+
+/*fonction création d'éléments*/
+function create(type, Qualified, nomType){
+    let nomVariable = document.createElement(type);
+    nomVariable.setAttribute(Qualified, nomType);
+    return nomVariable;
+}
