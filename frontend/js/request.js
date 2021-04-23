bbd710056e721c64c194111a3d879cd910ac9d55
KevinDruart <65339848+KevinDruart@users.noreply.github.com>
2021-04-23 16:40:45 +0200

    Revert "Revert "MAJ""
    This reverts commit 6e7ab04bd9e62d62aa02d54df84c0f502c579df5.

--- frontend/js/request.js
+++ frontend/js/request.js
@@ -1,10 +1,7 @@
-// URL API
-const url = "http://localhost:3000/api/teddies";
-const urlOrder = "http://localhost:3000/api/teddies/order";
-
-/*fonction création d'éléments*/
-function create(type, Qualified, nomType){
-    let nomVariable = document.createElement(type);
-    nomVariable.setAttribute(Qualified, nomType);
-    return nomVariable;
+const getRequest = async (url) => {
+    const response = await fetch(url);
+    return await response.json();
 }
+
+
+const url = "http://localhost:3000/api/teddies";
